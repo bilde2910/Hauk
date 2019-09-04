841285cd2e980523e393086354492c681abdbd3d
pachacamac <marcanbe@gmail.com>
2019-09-04 16:14:05 +0200

    add alternative light weight ruby backend without dependencies

---
+++ backend2/api.rb
@@ -0,0 +1,182 @@
+CONFIG = {
+  password: 'password',
+  public_url: 'https://localhost:9494',
+  ssl_cert: 'cert.crt',
+  ssl_pkey: 'pkey.pem',
+  port: 9494,
+  bind: '0.0.0.0',
+  session_store: 'sessions.json',
+  max_duration: 86400, # Maximum duration of a single location share, in seconds
+  min_interval: 1, # Minimum time between each location update, in seconds
+  max_cached_pts: 3, # Maximum number of data points stored for each share before old points are deleted. Map clients will see up to this amount of data points when they load the page.
+  max_shown_pts: 100, # Maximum number of data points that may be visible on the map at any time. This is used to draw trails behind the current location map marker. Higher values will show longer trails, but may reduce performance.
+  v_data_points: 2, # Number of seconds of data that should be used to calculate velocity
+  map_tile_uri: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  map_attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  default_zoom: 14, #(0-20), higher value means closer zooming.
+  max_zoom: 19,
+  trail_color: '#d80037',
+  velocity_unit: {
+    havMod: 6371 * 2 * 3600,
+    mpsMod: 3.6,
+    unit: "km/h"
+  }
+}
+
+#############################################################################################
+# Internal Service Setup
+#############################################################################################
+
+require 'time'
+require 'json'
+
+SESSIONS = File.exist?(CONFIG[:session_store]) ? JSON.parse(File.read(CONFIG[:session_store]), symbolize_names: true).tap{|s| puts "loaded #{s.size} session(s)"} : {}
+
+at_exit do
+  if CONFIG[:session_store]
+    puts 'saving sessions'
+    SESSIONS.delete_if {|k, v| v[:expire] < Time.now.to_i }
+    File.write(CONFIG[:session_store], SESSIONS.to_json)
+  end
+end
+
+require 'sinatra'
+require 'webrick/ssl'
+require 'webrick/https'
+
+SSL_CERT = CONFIG[:ssl_cert] && File.exist?(CONFIG[:ssl_cert]) ? CONFIG[:ssl_cert] : nil
+SSL_PKEY = CONFIG[:ssl_pkey] && File.exist?(CONFIG[:ssl_pkey]) ? CONFIG[:ssl_pkey] : nil
+
+module Sinatra
+  class Application
+    def self.run!
+      server_options = { Host: bind, Port: port }
+      if SSL_CERT && SSL_PKEY
+        server_options.merge!(
+          SSLEnable: true,
+          SSLCertificate: OpenSSL::X509::Certificate.new(File.open(SSL_CERT).read),
+          SSLPrivateKey: OpenSSL::PKey::RSA.new(File.open(SSL_PKEY).read) # Could add privKey password here
+        )
+      end
+
+      Rack::Handler::WEBrick.run self, server_options do |server|
+        [:INT, :TERM].each { |sig| trap(sig) { server.stop } }
+        server.threaded = settings.threaded if server.respond_to? :threaded=
+        set :running, true
+      end
+    end
+  end
+end
+
+unless SSL_CERT && SSL_PKEY
+  puts '##################################### WARNING ######################################'
+  puts 'No SSL certificate and privKey found or selected. Server starting without SSL.'
+  puts 'No worries! This is fine. But if you wanted to you could generate one by running:'
+  puts '> openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout pkey.pem -out cert.crt'
+  puts 'Or point to your existing cert and pkey in the config.'
+  puts '####################################################################################'
+  CONFIG[:public_url].gsub!('https:', 'http:')
+end
+
+set :port, CONFIG[:port] || 9494
+set :bind, CONFIG[:bind] || '0.0.0.0'
+
+def friendly_id
+  colors = ["amber", "beige", "black", "blue", "brown", "cyan", "gold", "green", "grey", "indigo", "lime", "magenta", "olive", "orange", "peach", "pink", "purple", "red", "rose", "silver", "teal", "white", "yellow"]
+  adjectives = ["amazing", "big", "cheeky", "cool", "cosmic", "dirty", "dotted", "easy", "fancy", "fat", "fresh", "fun", "genius", "ginger", "good", "kind", "loud", "mighty", "perky", "quick", "shiny", "short", "skinny", "sleek", "sleepy", "slow", "stinky", "sweet", "tall"]
+  nouns = ["ant", "bee", "cat", "dog", "elk", "fox", "gnu", "hog", "pig", "rat", "yak"]
+  num = rand(10...99)
+  "#{colors.sample}-#{adjectives.sample}-#{nouns.sample}-#{num}"
+end
+
+#############################################################################################
+# Endpoints
+#############################################################################################
+
+get '/' do
+  send_file File.join(settings.public_folder, 'index.html')
+end
+
+get '/dynamic.js.php' do
+  content_type :js
+  <<-EOS
+  window.TILE_URI = '#{CONFIG[:map_tile_uri]}';
+  window.ATTRIBUTION = '#{CONFIG[:map_attribution]}';
+  window.DEFAULT_ZOOM = #{CONFIG[:default_zoom]};
+  window.MAX_ZOOM = #{CONFIG[:max_zoom]};
+  window.MAX_POINTS = #{CONFIG[:max_shown_pts]};
+  window.VELOCITY_DELTA_TIME = #{CONFIG[:v_data_points]};
+  window.TRAIL_COLOR = '#{CONFIG[:trail_color]}';
+  window.VELOCITY_UNIT = #{CONFIG[:velocity_unit].to_json};
+  EOS
+end
+
+# Handles session initiation for Hauk clients.
+# Creates a session ID and associated share URL, and returns these to the client.
+# Test: curl -d 'pwd=password&dur=60&int=1' localhost:9494/api/create.php
+post '/api/create.php' do
+  [:pwd, :dur, :int].each { |e| halt(400, "Missing data\n") if !params[e] }
+  halt(403, "Incorrect password!\n") if params[:pwd] != CONFIG[:password]
+  duration = params[:dur].to_i
+  interval = params[:int].to_f
+  halt(400, "Share period is too long!\n") if duration > CONFIG[:max_duration]
+  halt(400, "Ping interval is too long!\n") if interval > CONFIG[:max_duration]
+  halt(400, "Ping interval is too short!\n") if interval < CONFIG[:min_interval]
+  # TODO: create nicer session ids
+  #loop while SESSIONS[ sid = SecureRandom.random_number(2**256).to_s(36).ljust(6,'a')[0...6].to_sym ]
+  loop while SESSIONS[sid = friendly_id.to_sym]
+  SESSIONS[sid] = {
+    expire: Time.now.to_i + duration,
+    interval: interval,
+    locations: []
+  }
+  "OK\n#{sid}\n#{CONFIG[:public_url]}?#{sid}\n"
+end
+
+# Called by the client to receive location updates. A link ID is required to retrieve data.
+# Test: curl localhost:9494/api/fetch.php?id=xxxxxx
+get '/api/fetch.php' do
+  sid = params[:id] # XXX: not so nice that it's called id here but sid in all other entpoints. wanted to keep compatibility though.
+  SESSIONS.delete_if {|k, v| v[:expire] < Time.now.to_i }
+  if SESSIONS[sid.to_sym]
+    content_type :json
+    {
+      i: SESSIONS[sid.to_sym][:interval],
+      x: SESSIONS[sid.to_sym][:expire],
+      l: SESSIONS[sid.to_sym][:locations]
+    }.to_json
+  else
+    halt(404, "Invalid session!\n")
+  end
+end
+
+# Called from the Hauk app to push location updates to the server.
+# Each update contains a location and timestamp from when the location was fetched by the client.
+# Test: curl -d 'lat=52.52&lon=13.40&time=1567602256.994&sid=xxxxxx' localhost:9494/api/post.php
+post '/api/post.php' do
+  [:lat, :lon, :time, :sid].each { |e| halt(400, "Missing data\n") if !params[e] }
+  lat = params[:lat].to_f
+  lon = params[:lon].to_f
+  time = params[:time].to_f
+  halt(400, "Invalid location!\n") if lat < -90 || lat > 90 || lon < -180 || lon > 180
+  # Not all devices report speed and accuracy, but if available, report them too.
+  speed = params[:spd] ? params[:spd].to_f : nil
+  accuracy = params[:acc] ? params[:acc].to_f : nil
+  sid = params[:sid]
+  SESSIONS.delete_if {|k, v| v[:expire] < Time.now.to_i }
+  if SESSIONS[sid.to_sym]
+    SESSIONS[sid.to_sym][:locations] << [lat, lon, time, accuracy, speed]
+    SESSIONS[sid.to_sym][:locations].shift(SESSIONS[sid.to_sym][:locations].size - CONFIG[:max_cached_pts]) if SESSIONS[sid.to_sym][:locations].size > CONFIG[:max_cached_pts]
+    "OK\n"
+  else
+    halt(404, "Session expired!\n")
+  end
+end
+
+# Handles session cancellation for Hauk clients.
+# Test:curl -d 'sid=xxxxxx' localhost:9494/api/stop.php
+post '/api/stop.php' do
+  halt(400, "Missing data\n") if !params[:sid]
+  SESSIONS.delete(params[:sid].to_sym)
+  "OK\n";
+end
---
+++ backend2/public/main.js
@@ -0,0 +1,231 @@
+// This is the main script file for Hauk's web view client.
+
+// Create a Leaflet map.
+var map = L.map('map').setView([0, 0], DEFAULT_ZOOM);
+L.tileLayer(TILE_URI, {
+    attribution: ATTRIBUTION,
+    maxZoom: MAX_ZOOM
+}).addTo(map);
+
+var circleLayer = L.layerGroup().addTo(map);
+var markerLayer = L.layerGroup().addTo(map);
+
+// A list of points received from the server.
+var points = [];
+
+// The leaflet marker.
+var marker = null;
+var icon = null;
+var circle = null;
+
+// Retrieve the sharing link ID from the URL. E.g.
+// https://example.com/?ABCD-1234 --> "ABCD-1234"
+var id = location.href.substr(location.href.indexOf("?") + 1);
+if (id.indexOf("&") !== -1) id = id.substr(0, id.indexOf("&"));
+
+function getJSON(url, callback, invalid) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status === 200) {
+            try {
+                var json = JSON.parse(this.responseText);
+                callback(json);
+            } catch (ex) {
+                console.log(ex);
+                invalid();
+            }
+        }
+    }
+    xhr.send();
+}
+
+document.getElementById("dismiss").addEventListener("click", function() {
+    document.getElementById("expired").style.display = "none";
+});
+
+// Attempt to fetch location data from the server once.
+getJSON("./api/fetch.php?id=" + id, function(data) {
+    document.getElementById("mapouter").style.visibility = "visible";
+
+    // The data contains an expiration time. Create a countdown at the top of
+    // the map screen that ends when the share is over.
+    var interval2 = setInterval(function() {
+        var seconds = data.x - Math.round(Date.now() / 1000);
+        if (seconds < 0) {
+            clearInterval(interval2);
+            return;
+        }
+
+        var h = Math.floor(seconds / 3600);
+        var m = Math.floor((seconds % 3600) / 60);
+        var s = seconds % 60;
+
+        var time = "";
+        if (h > 0) time += h + ":";
+        if (h > 0 && m < 10) time += "0";
+        time += m + ":";
+        if (s < 10) time += "0";
+        time += s;
+
+        document.getElementById("countdown").textContent = time;
+    }, 1000);
+
+    // The location data contains an interval. Schedule a task that fetches data
+    // once per interval time.
+    var interval = setInterval(function() {
+        // Stop the task if the share has expired.
+        if ((Date.now() / 1000) >= data.x) {
+            clearInterval(interval);
+            clearInterval(interval2);
+            document.getElementById("countdown").textContent = "Expired";
+            document.getElementById("expired").style.display = "block";
+        }
+
+        getJSON("./api/fetch.php?id=" + id, function(data) {
+            processUpdate(data);
+        }, function() {
+            clearInterval(interval);
+            clearInterval(interval2);
+            document.getElementById("countdown").textContent = "Expired";
+            document.getElementById("expired").style.display = "block";
+        });
+    }, data.i * 1000);
+
+
+
+    processUpdate(data);
+}, function() {
+    document.getElementById("notfound").style.display = "block";
+});
+
+// Parses the data returned from ./api/fetch.php and updates the map marker.
+function processUpdate(data) {
+    // Get the last location received.
+    var lastPoint = points.length > 0 ? points[points.length - 1] : null;
+
+    for (var i = 0; i < data.l.length; i++) {
+        var lat = data.l[i][0];
+        var lon = data.l[i][1];
+        var time = data.l[i][2];
+        var acc = data.l[i][3];
+        var spd = data.l[i][4];
+
+        // Check if the location should be added. Only add new location points
+        // if the point was not recorded before the last recorded point.
+        if (lastPoint === null || time > lastPoint.time) {
+            var line = null;
+            if (marker == null) {
+                // Add a marker to the map if it's not already there.
+                icon = L.divIcon({
+                    html: '<div id="marker"><div id="arrow"></div><p><span id="velocity">0.0</span> ' + VELOCITY_UNIT.unit + '</p></div>',
+                    iconAnchor: [33, 18]
+                });
+                marker = L.marker([lat, lon], {icon: icon, interactive: false}).addTo(markerLayer);
+            } else {
+                // If there is a marker, draw a line from its last location
+                // instead and move the marker.
+                line = L.polyline([marker.getLatLng(), [lat, lon]], {color: TRAIL_COLOR}).addTo(markerLayer);
+                marker.setLatLng([lat, lon]);
+            }
+            // Draw an accuracy circle if GPS accuracy was provided by the
+            // client.
+            if (acc !== null && circle == null) {
+                circle = L.circle([lat, lon], {radius: acc, fillColor: '#d80037', fillOpacity: 0.25, color: '#d80037', opacity: 0.5, interactive: false}).addTo(circleLayer);
+            } else if (circle !== null) {
+                circle.setLatLng([lat, lon]);
+                if (acc !== null) circle.setRadius(acc);
+            }
+            points.push({lat: lat, lon: lon, line: line, time: time, spd: spd, acc: acc});
+            lastPoint = points[points.length - 1];
+        }
+    }
+
+    var eVelocity = document.getElementById("velocity")
+    if (lastPoint !== null && lastPoint.spd !== null && eVelocity !== null) {
+        // Prefer client-provided speed if possible.
+        eVelocity.textContent = (lastPoint.spd * VELOCITY_UNIT.mpsMod).toFixed(1);
+    } else if (eVelocity !== null) {
+        // If the client didn't provide its speed, calculate it locally from its
+        // list of locations.
+        var dist = 0;
+        var time = 0;
+        var idx = points.length;
+
+        // Iterate over all locations backwards until we either reach our
+        // required VELOCITY_DELTA_TIME, or we run out of points.
+        while (idx > 2) {
+            idx--;
+            var pt1 = points[idx - 1];
+            var pt2 = points[idx];
+            var dTime = pt2.time - pt1.time;
+
+            // If the new time does not exceed the VELOCITY_DELTA_TIME, add the
+            // time and distance deltas to the appropriate sum for averaging;
+            // otherwise, break the loop and proceed to calculate.
+            if (time + dTime <= VELOCITY_DELTA_TIME) {
+                time += dTime;
+                dist += distance(pt1, pt2);
+            } else {
+                break;
+            }
+        }
+
+        // Update the UI with the velocity.
+        eVelocity.textContent = velocity(dist, time);
+    }
+
+
+    // Follow the marker.
+    if (lastPoint !== null) map.panTo([lastPoint.lat, lastPoint.lon]);
+
+    // Rotate the marker to the direction of movement.
+    var eArrow = document.getElementById("arrow");
+    if (eArrow !== null && points.length >= 2) {
+        var last = points.length - 1;
+        eArrow.style.transform = "rotate(" + angle(points[last - 1], points[last]) + "deg)";
+    }
+
+    // Prune the array of locations so it does not exceed our MAX_POINTS defined
+    // in the config.
+    if (points.length > MAX_POINTS) {
+        var remove = points.splice(0, points.length - MAX_POINTS);
+        for (var j = 0; j < remove.length; j++) if (remove[j].line !== null) map.removeLayer(remove[j].line);
+    }
+}
+
+// Calculates the distance between two points on a sphere using the Haversine
+// algorithm.
+function distance(from, to) {
+    var d2r = Math.PI / 180;
+    var fla = from.lat * d2r, tla = to.lat * d2r;
+    var flo = from.lon * d2r, tlo = to.lon * d2r;
+    var havLat = Math.sin((tla - fla) / 2); havLat *= havLat;
+    var havLon = Math.sin((tlo - flo) / 2); havLon *= havLon;
+    var hav = havLat + Math.cos(fla) * Math.cos(tla) * havLon;
+    var d = Math.asin(Math.sqrt(hav));
+    return d;
+}
+
+// Calculates a velocity using the velocity unit from the config.
+function velocity(distance, intv) {
+    if (intv == 0) return "0.0";
+    return (distance * VELOCITY_UNIT.havMod / intv).toFixed(1);
+}
+
+// Calculates the bearing between two points on a sphere in degrees.
+function angle(from, to) {
+    var d2r = Math.PI / 180;
+    var fromLat = from.lat * d2r, toLat = to.lat * d2r;
+    var fromLon = from.lon * d2r, toLon = to.lon * d2r;
+    /*
+        Calculation code by krishnar from
+        https://stackoverflow.com/a/52079217
+    */
+    var x = Math.cos(fromLat) * Math.sin(toLat)
+          - Math.sin(fromLat) * Math.cos(toLat) * Math.cos(toLon - fromLon);
+    var y = Math.sin(toLon - fromLon) * Math.cos(toLat);
+    var heading = Math.atan2(y, x) / d2r;
+
+    return (heading + 360) % 360;
+}
