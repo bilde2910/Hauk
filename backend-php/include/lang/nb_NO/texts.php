8b4981fcf46a532021b8af37ef6b940f3073d589
Marius Lindvall <marius@varden.info>
2019-10-11 16:35:35 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Hauk/PHP backend
    Translate-URL: https://traduki.varden.info/projects/hauk/backend-php/nb_NO/

--- backend-php/include/lang/nb_NO/texts.php
+++ backend-php/include/lang/nb_NO/texts.php
@@ -1,1 +1,17 @@
 <?php
+$LANG['no_redis_ext'] = 'Ingen kompatibel redis-utvidelse (redis) er aktivert i PHP-konfigurasjonen din!';
+$LANG['invalid_storage'] = 'Du har satt en ugyldig storage_backend i Hauk!';
+$LANG['session_expired'] = 'Sesjon utløpt!';
+$LANG['share_not_found'] = 'Den gitte delingen finnes ikke!';
+$LANG['group_share_not_adoptable'] = 'Du kan ikke adoptere delingsgrupper!';
+$LANG['share_adoption_not_allowed'] = 'Eieren av den gitte delingen tillater ikke adopsjon!';
+$LANG['incorrect_password'] = 'Feil passord!';
+$LANG['share_too_long'] = 'Delingsperioden er for lang!';
+$LANG['interval_too_long'] = 'Delingsintervallet er for langt!';
+$LANG['interval_too_short'] = 'Delingsintervallet er for kort!';
+$LANG['share_mode_unsupported'] = 'Delingsmodusen støttes ikke!';
+$LANG['group_pin_invalid'] = 'Ugyldig gruppekode!';
+$LANG['session_invalid'] = 'Ugyldig sesjon!';
+$LANG['location_invalid'] = 'Ugyldig lokasjon!';
+$LANG['config_missing'] = 'Kunne ikke finne config.php!';
+$LANG['no_memcached_ext'] = 'Ingen kompatibel memcached-utvidelse (memcache eller memcached) er aktivert i PHP-konfigurasjonen din!';
