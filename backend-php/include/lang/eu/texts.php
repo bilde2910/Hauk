4587be7949785bc65f7e2e50ce0d6aad451f3dbe
Osoitz <oelkoro@gmail.com>
2019-10-13 17:33:24 +0000

    Translated using Weblate (Basque)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Hauk/PHP backend
    Translate-URL: https://traduki.varden.info/projects/hauk/backend-php/eu/

--- backend-php/include/lang/eu/texts.php
+++ backend-php/include/lang/eu/texts.php
@@ -1,1 +1,17 @@
 <?php
+$LANG['config_missing'] = 'Ezin izan da config.php aurkitu!';
+$LANG['no_memcached_ext'] = 'Ez dago memcached luzapen bateragarririk (memcache edo memcached) gaituta zure PHP konfigurazioan!';
+$LANG['no_redis_ext'] = 'Ez dago redis luzapen bateragarririk (redis) gaituta zure PHP konfigurazioan!';
+$LANG['invalid_storage'] = 'storage_backend baliogabe bat ezarri duzu Hauk zerbitzarian!';
+$LANG['session_expired'] = 'Saioak iraungi du!';
+$LANG['share_not_found'] = 'Emandako partekatzea ez da existitzen!';
+$LANG['group_share_not_adoptable'] = 'Ezin dituzu talde partekatzeak adoptatu!';
+$LANG['share_adoption_not_allowed'] = 'Emandako partekatzearen ostalariak ez du adopzioa baimentzen!';
+$LANG['incorrect_password'] = 'Pasahitz okerra!';
+$LANG['share_too_long'] = 'Partekatze denbora luzeegia da!';
+$LANG['interval_too_long'] = 'Ping tartea luzeegia da!';
+$LANG['interval_too_short'] = 'Ping tartea laburregia da!';
+$LANG['share_mode_unsupported'] = 'Euskarririk gabeko parekatze modua!';
+$LANG['group_pin_invalid'] = 'Baliogabeko talde PIN-a!';
+$LANG['session_invalid'] = 'Saio baliogabea!';
+$LANG['location_invalid'] = 'Kokaleku baliogabea!';
