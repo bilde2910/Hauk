cad6c13f704fe66cf26c4b759ca202a16bdde453
Stefan <natrius@mailbox.org>
2019-10-18 09:15:30 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Hauk/PHP backend
    Translate-URL: https://traduki.varden.info/projects/hauk/backend-php/de/

--- backend-php/include/lang/de/texts.php
+++ backend-php/include/lang/de/texts.php
@@ -1,1 +1,17 @@
 <?php
+$LANG['location_invalid'] = 'Ungültiger Standort!';
+$LANG['session_invalid'] = 'Ungültige Sitzung!';
+$LANG['group_pin_invalid'] = 'Ungültiger Gruppen PIN!';
+$LANG['share_mode_unsupported'] = 'Nicht unterstützter Freigabemodus!';
+$LANG['interval_too_short'] = 'Der Ping-Intervall ist zu kurz!';
+$LANG['interval_too_long'] = 'Der Ping-Intervall ist zu lang!';
+$LANG['share_too_long'] = 'Der Freigabezeitraum ist zu lang!';
+$LANG['incorrect_password'] = 'Falsches Passwort!';
+$LANG['share_adoption_not_allowed'] = 'Der Host der angegebenen Freigabe erlaubt keine Übernahme!';
+$LANG['group_share_not_adoptable'] = 'Sie können keine Gruppenfreigabe übernehmen!';
+$LANG['share_not_found'] = 'Die angegebene Freigabe existiert nicht!';
+$LANG['session_expired'] = 'Sitzung abgelaufen!';
+$LANG['invalid_storage'] = 'Ein ungültiges storage_backend wurde in Hauk gesetzt!';
+$LANG['no_redis_ext'] = 'Es ist keine kompatible redis Erweiterung (redis) in Ihrer PHP-Konfiguration aktiviert!';
+$LANG['no_memcached_ext'] = 'Es ist keine kompatible memcached Erweiterung (memcache oder memcached) in Ihrer PHP-Konfiguration aktiviert!';
+$LANG['config_missing'] = 'config.php konnte nicht gefunden werden!';
