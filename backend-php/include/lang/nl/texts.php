ab1ab677c89d963e0b316df1e1f335fc97b601c4
Jdekoning141 <J.dekoning141@gmail.com>
2019-10-19 22:30:04 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Hauk/PHP backend
    Translate-URL: https://traduki.varden.info/projects/hauk/backend-php/nl/

--- backend-php/include/lang/nl/texts.php
+++ backend-php/include/lang/nl/texts.php
@@ -1,1 +1,17 @@
 <?php
+$LANG['session_expired'] = 'Sessie verlopen!';
+$LANG['share_not_found'] = 'De ingevoerde locatiedeling bestaat niet!';
+$LANG['group_share_not_adoptable'] = 'Groepsdelingen kunnen niet geadopteerd worden!';
+$LANG['incorrect_password'] = 'Incorrect wachtwoord!';
+$LANG['share_too_long'] = 'Deelperiode is te lang!';
+$LANG['interval_too_long'] = 'Ping frequentie is te lang!';
+$LANG['group_pin_invalid'] = 'Ongeldige groep PIN!';
+$LANG['location_invalid'] = 'Ongeldige locatie!';
+$LANG['config_missing'] = 'Kan config.php niet vinden!';
+$LANG['interval_too_short'] = 'Ping frequentie is te kort!';
+$LANG['no_memcached_ext'] = 'Geen compatibele memcached extensie (memcache of memcached) is ingeschakeld in uw PHP configuratie!';
+$LANG['share_mode_unsupported'] = 'Niet ondersteunde deelmodus!';
+$LANG['no_redis_ext'] = 'Geen compatibele redis extensie (redis) is ingeschakeld in uw PHP configuratie!';
+$LANG['session_invalid'] = 'Ongeldige sessie!';
+$LANG['invalid_storage'] = 'U heeft een ongeldige \'storage_backend\' ingesteld in Hauk!';
+$LANG['share_adoption_not_allowed'] = 'De eigenaar van de ingevoerde deling staat adoptie niet toe!';
