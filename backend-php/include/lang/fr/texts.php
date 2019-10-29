e25fa503634505b0a3bc3dfe09bd2ffd1e0ab870
Thibault Francois <thifranc42@gmail.com>
2019-10-29 13:04:08 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Hauk/PHP backend
    Translate-URL: https://traduki.varden.info/projects/hauk/backend-php/fr/

--- backend-php/include/lang/fr/texts.php
+++ backend-php/include/lang/fr/texts.php
@@ -1,1 +1,17 @@
 <?php
+$LANG['location_invalid'] = 'Localisation invalide !';
+$LANG['session_invalid'] = 'Session invalide !';
+$LANG['group_pin_invalid'] = 'Groupe PIN invalide !';
+$LANG['share_mode_unsupported'] = 'Mode de partage non supporté !';
+$LANG['interval_too_short'] = 'Intervalle de ping trop courte !';
+$LANG['interval_too_long'] = 'Intervalle de ping trop longue !';
+$LANG['share_too_long'] = 'Période de partage trop longue !';
+$LANG['incorrect_password'] = 'Mot de passe invalide !';
+$LANG['share_adoption_not_allowed'] = 'L\'hôte du partage renseigné n\'accepte pas l\'ajout de partage !';
+$LANG['group_share_not_adoptable'] = 'Vous ne pouvez pas ajouter de partage de groupe !';
+$LANG['share_not_found'] = 'Le partage donné n\'existe pas !';
+$LANG['session_expired'] = 'Session expirée !';
+$LANG['invalid_storage'] = 'Vous avez paramétré un storage_backend invalide dans Hauk !';
+$LANG['no_redis_ext'] = 'Aucune extension redis ( redis ) compatible n\'est authorisée dans votre configuration PHP !';
+$LANG['no_memcached_ext'] = 'Aucune extension memcached ( memcache ou memcached ) n\'est authorisée dans votre configuration PHP !';
+$LANG['config_missing'] = 'Fichier config.php non trouvé !';
