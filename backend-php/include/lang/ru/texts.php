60be6fa5bc369b4e9fae5f7c03789e79ebbab1df
RuralYak <dpo.scanner@gmail.com>
2019-11-06 01:27:01 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Hauk/PHP backend
    Translate-URL: https://traduki.varden.info/projects/hauk/backend-php/ru/

--- backend-php/include/lang/ru/texts.php
+++ backend-php/include/lang/ru/texts.php
@@ -1,1 +1,17 @@
 <?php
+$LANG['location_invalid'] = 'Неверное местоположение!';
+$LANG['session_invalid'] = 'Неверная сессия!';
+$LANG['group_pin_invalid'] = 'Неверный групповой PIN!';
+$LANG['share_mode_unsupported'] = 'Неправильный режим шары!';
+$LANG['interval_too_short'] = 'Слишкмо малое время пинга!';
+$LANG['interval_too_long'] = 'Слишком большое время пинга!';
+$LANG['share_too_long'] = 'Слишком длинное время жизни шары!';
+$LANG['incorrect_password'] = 'Неверный пароль!';
+$LANG['share_adoption_not_allowed'] = 'Сервер этой шары не позволяет добавлять сессии!';
+$LANG['group_share_not_adoptable'] = 'Не могу добавить групповую шару!';
+$LANG['share_not_found'] = 'Данная шара уже не существует!';
+$LANG['session_expired'] = 'Сессия истекла!';
+$LANG['invalid_storage'] = 'У вас неверный storage_backend для Hauk!';
+$LANG['no_redis_ext'] = 'Нет включенных совместимых расширений redis в вашей конфигурации PHP!';
+$LANG['no_memcached_ext'] = 'Нет включенных совместимых расширений в вашей конфигурации PHP (memcache or memcached)!';
+$LANG['config_missing'] = 'Не могу найти config.php!';
