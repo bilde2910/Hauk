054d8c7214201b55f006fb0e8fbffc4a517bba7a
RuralYak <dpo.scanner@gmail.com>
2019-11-06 03:33:11 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Hauk/PHP backend
    Translate-URL: https://traduki.varden.info/projects/hauk/backend-php/uk/

--- backend-php/include/lang/uk/texts.php
+++ backend-php/include/lang/uk/texts.php
@@ -1,1 +1,17 @@
 <?php
+$LANG['no_memcached_ext'] = 'У вашій PHP-конфігурації не включено сумісне розширення (memcache або memcached)!';
+$LANG['no_redis_ext'] = 'У вашій PHP-конфігурації не включено сумісне розширення Redis (redis)!';
+$LANG['invalid_storage'] = 'Ви встановили недійсний storage_backend у Hauk!';
+$LANG['session_expired'] = 'Термін дії сесії закінчився!';
+$LANG['share_not_found'] = 'Даної шари не існує!';
+$LANG['group_share_not_adoptable'] = 'Ви не можете додавати групові шари!';
+$LANG['share_adoption_not_allowed'] = 'Хост цієї шари не дозволяє додавання!';
+$LANG['incorrect_password'] = 'Невірний пароль!';
+$LANG['share_too_long'] = 'Час життя шари завеликий!';
+$LANG['interval_too_long'] = 'Інтервал пінга завеликий!';
+$LANG['interval_too_short'] = 'Інтервал пінга замалий!';
+$LANG['share_mode_unsupported'] = 'Непідтримуваний режим шари!';
+$LANG['group_pin_invalid'] = 'Невірний груповий PIN!';
+$LANG['session_invalid'] = 'Недійсний сеанс!';
+$LANG['location_invalid'] = 'Недійсне місцезнаходження!';
+$LANG['config_missing'] = 'Не можу знайти config.php!';
