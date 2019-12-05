3305f4956edee52eb95ebb247cabacbca21d7a26
Marius Lindvall <marius@varden.info>
2019-12-05 20:44:04 +0100

    Improve logging format

--- android/app/src/main/java/info/varden/hauk/utils/Log.java
+++ android/app/src/main/java/info/varden/hauk/utils/Log.java
@@ -1,5 +1,7 @@
 package info.varden.hauk.utils;
 
+import info.varden.hauk.BuildConfig;
+
 /**
  * Log wrapper to simplify logging in Hauk.
  *
@@ -14,7 +16,11 @@ public enum Log {
      * Returns the caller of the log function.
      */
     private static String getCaller() {
-        return Thread.currentThread().getStackTrace()[STACK_DEPTH].toString();
+        String caller = Thread.currentThread().getStackTrace()[STACK_DEPTH].toString();
+        if (caller.startsWith(BuildConfig.APPLICATION_ID)) {
+            caller = caller.substring(BuildConfig.APPLICATION_ID.length());
+        }
+        return caller;
     }
 
     /**
@@ -31,98 +37,98 @@ public enum Log {
     }
 
     public static void e(String msg) {
-        android.util.Log.e(getCaller(), msg);
+        android.util.Log.e(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg);
     }
 
     public static void e(String msg, Object... args) {
-        android.util.Log.e(getCaller(), String.format(msg, argsToStrings(args)));
+        android.util.Log.e(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)));
     }
 
     public static void e(String msg, Throwable tr) {
-        android.util.Log.e(getCaller(), msg, tr);
+        android.util.Log.e(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg, tr);
     }
 
     public static void e(String msg, Throwable tr, Object... args) {
-        android.util.Log.e(getCaller(), String.format(msg, argsToStrings(args)), tr);
+        android.util.Log.e(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)), tr);
     }
 
     public static void w(String msg) {
-        android.util.Log.w(getCaller(), msg);
+        android.util.Log.w(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg);
     }
 
     public static void w(String msg, Object... args) {
-        android.util.Log.w(getCaller(), String.format(msg, argsToStrings(args)));
+        android.util.Log.w(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)));
     }
 
     public static void w(String msg, Throwable tr) {
-        android.util.Log.w(getCaller(), msg, tr);
+        android.util.Log.w(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg, tr);
     }
 
     public static void w(String msg, Throwable tr, Object... args) {
-        android.util.Log.w(getCaller(), String.format(msg, argsToStrings(args)), tr);
+        android.util.Log.w(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)), tr);
     }
 
     public static void i(String msg) {
-        android.util.Log.i(getCaller(), msg);
+        android.util.Log.i(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg);
     }
 
     public static void i(String msg, Object... args) {
-        android.util.Log.i(getCaller(), String.format(msg, argsToStrings(args)));
+        android.util.Log.i(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)));
     }
 
     public static void i(String msg, Throwable tr) {
-        android.util.Log.i(getCaller(), msg, tr);
+        android.util.Log.i(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg, tr);
     }
 
     public static void i(String msg, Throwable tr, Object... args) {
-        android.util.Log.i(getCaller(), String.format(msg, argsToStrings(args)), tr);
+        android.util.Log.i(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)), tr);
     }
 
     public static void v(String msg) {
-        android.util.Log.v(getCaller(), msg);
+        android.util.Log.v(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg);
     }
 
     public static void v(String msg, Object... args) {
-        android.util.Log.v(getCaller(), String.format(msg, argsToStrings(args)));
+        android.util.Log.v(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)));
     }
 
     public static void v(String msg, Throwable tr) {
-        android.util.Log.v(getCaller(), msg, tr);
+        android.util.Log.v(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg, tr);
     }
 
     public static void v(String msg, Throwable tr, Object... args) {
-        android.util.Log.v(getCaller(), String.format(msg, argsToStrings(args)), tr);
+        android.util.Log.v(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)), tr);
     }
 
     public static void d(String msg) {
-        android.util.Log.d(getCaller(), msg);
+        android.util.Log.d(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg);
     }
 
     public static void d(String msg, Object... args) {
-        android.util.Log.d(getCaller(), String.format(msg, argsToStrings(args)));
+        android.util.Log.d(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)));
     }
 
     public static void d(String msg, Throwable tr) {
-        android.util.Log.v(getCaller(), msg, tr);
+        android.util.Log.v(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg, tr);
     }
 
     public static void d(String msg, Throwable tr, Object... args) {
-        android.util.Log.d(getCaller(), String.format(msg, argsToStrings(args)), tr);
+        android.util.Log.d(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)), tr);
     }
 
     public static void wtf(String msg) {
-        android.util.Log.wtf(getCaller(), msg);
+        android.util.Log.wtf(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg);
     }
 
     public static void wtf(String msg, Object... args) {
-        android.util.Log.wtf(getCaller(), String.format(msg, argsToStrings(args)));
+        android.util.Log.wtf(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)));
     }
 
     public static void wtf(String msg, Throwable tr) {
-        android.util.Log.wtf(getCaller(), msg, tr);
+        android.util.Log.wtf(BuildConfig.APPLICATION_ID, getCaller() + ": " + msg, tr);
     }
 
     public static void wtf(String msg, Throwable tr, Object... args) {
-        android.util.Log.wtf(getCaller(), String.format(msg, argsToStrings(args)), tr);
+        android.util.Log.wtf(BuildConfig.APPLICATION_ID, getCaller() + ": " + String.format(msg, argsToStrings(args)), tr);
     }
 }
