eddd168b1664fff83e11a662c15d31cea3cb79e4
Jordi Borràs i Vivó <jordibv@gmail.com>
2019-12-09 12:16:03 +0000

    Translated using Weblate (Catalan)
    Currently translated at 100.0% (19 of 19 strings)
    
    Translation: Hauk/PHP backend
    Translate-URL: https://traduki.varden.info/projects/hauk/backend-php/ca/

--- backend-php/include/lang/ca/texts.php
+++ backend-php/include/lang/ca/texts.php
@@ -1,1 +1,20 @@
 <?php
+$LANG['e2e_adoption_not_allowed'] = 'Aquesta compartició està usant el xifrat d\'extrem a extrem i no pot ser adoptat!';
+$LANG['group_e2e_unsupported'] = 'El xifrat d\'extrem a extrem no es pot fer servir per les comparticions de grup!';
+$LANG['location_invalid'] = 'Localització invàlida!';
+$LANG['session_invalid'] = 'Sessió invàlida!';
+$LANG['group_pin_invalid'] = 'PIN de grup no vàlid!';
+$LANG['share_mode_unsupported'] = 'Mode de compartició no suportat!';
+$LANG['interval_too_short'] = 'L\'interval de ping és massa curt!';
+$LANG['interval_too_long'] = 'L\'interval de ping és massa llarg!';
+$LANG['share_too_long'] = 'El període de compartició és massa llarg!';
+$LANG['username_required'] = 'Nom d\'usuari requerit!';
+$LANG['incorrect_password'] = 'Password incorrecte!';
+$LANG['share_adoption_not_allowed'] = 'L\'amfitrió de la compartició donada no permet l\'adopció!';
+$LANG['group_share_not_adoptable'] = 'No podeu adoptar comparticions de grup!';
+$LANG['share_not_found'] = 'La compartició donada no existeix!';
+$LANG['session_expired'] = 'La sessió ha expirat!';
+$LANG['invalid_storage'] = 'Heu establert un storage_backend d\'emmagatzematge no vàlid a Hauk!';
+$LANG['no_redis_ext'] = 'No hi ha habilitada cap extensió redis compatible (redis) a la vostra configuració PHP!';
+$LANG['no_memcached_ext'] = 'No hi ha habilitada cap extensió confidencial (memcache o memcached) a la configuració de PHP!';
+$LANG['config_missing'] = 'No es pot trobar config.php!';
