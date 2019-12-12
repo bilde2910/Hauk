9b811869cf078cc3bb082d4524016229212ab302
Licaon_Kter <licaon.kter@protonmail.com>
2019-12-12 11:22:21 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (19 of 19 strings)
    
    Translation: Hauk/PHP backend
    Translate-URL: https://traduki.varden.info/projects/hauk/backend-php/ro/

--- backend-php/include/lang/ro/texts.php
+++ backend-php/include/lang/ro/texts.php
@@ -17,3 +17,4 @@ $LANG['share_not_found'] = 'Această partajare nu există!';
 $LANG['session_expired'] = 'Sesiune expirată!';
 $LANG['config_missing'] = 'Nu s-a putut găsi config.php!';
 $LANG['no_redis_ext'] = 'În setările PHP nu aveți nici o extensie redis (redis) compatibilă activă!';
+$LANG['invalid_storage'] = 'Ați setat în Hauk un storage_backend invalid!';
