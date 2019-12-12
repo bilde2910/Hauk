5c21d4bb7dcbc37c44b6cb3661ac3f367fa0db09
Licaon_Kter <licaon.kter@protonmail.com>
2019-12-12 10:23:17 +0000

    Translated using Weblate (Romanian)
    Currently translated at 89.5% (17 of 19 strings)
    
    Translation: Hauk/PHP backend
    Translate-URL: https://traduki.varden.info/projects/hauk/backend-php/ro/

--- backend-php/include/lang/ro/texts.php
+++ backend-php/include/lang/ro/texts.php
@@ -1,1 +1,18 @@
 <?php
+$LANG['no_memcached_ext'] = 'În setările PHP nu aveți nici o extensie memcached (memcache sau memcached) compatibilă!';
+$LANG['e2e_adoption_not_allowed'] = 'Această partajare este protejată prin parolă și nu poate fi adoptată!';
+$LANG['group_e2e_unsupported'] = 'Partajările de grup nu pot fi protejate prin parolă!';
+$LANG['location_invalid'] = 'Locație invalidă!';
+$LANG['session_invalid'] = 'Sesiune invalidă!';
+$LANG['group_pin_invalid'] = 'PIN de grup invalid!';
+$LANG['share_mode_unsupported'] = 'Mod de partajare nesuportat!';
+$LANG['interval_too_short'] = 'Intervalul de actualizare este prea scurt!';
+$LANG['interval_too_long'] = 'Intervalul de actualizare este prea lung!';
+$LANG['share_too_long'] = 'Perioada de partajare este prea lungă!';
+$LANG['username_required'] = 'Nume de utilizator necesar!';
+$LANG['incorrect_password'] = 'Parolă incorectă!';
+$LANG['share_adoption_not_allowed'] = 'Gazda acestei partajări nu permite adopția!';
+$LANG['group_share_not_adoptable'] = 'Nu puteți adopta partajări de grup!';
+$LANG['share_not_found'] = 'Această partajare nu există!';
+$LANG['session_expired'] = 'Sesiune expirată!';
+$LANG['config_missing'] = 'Nu s-a putut găsi config.php!';
